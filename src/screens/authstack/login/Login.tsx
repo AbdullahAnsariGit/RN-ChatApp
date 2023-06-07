16b84772afbf6b5ea841a36fb8d3a25eccbbf711
AbdullahAnsariBinate <abdullah.ansari@binatedigital.com>
2023-06-07 07:33:31 +0500

    chore: firebase auth done

--- src/screens/authstack/login/Login.tsx
+++ src/screens/authstack/login/Login.tsx
@@ -1,5 +1,5 @@
 import { SafeAreaView, View } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Form } from './Form'
 import authStyles from '../authStyles'
 import FastImage from 'react-native-fast-image'
@@ -9,26 +9,55 @@ import { Pressable } from 'react-native'
 import { Text } from 'react-native'
 import * as NavigationService from "react-navigation-helpers";
 import { SCREENS } from '@shared-constants'
+import firestore from '@react-native-firebase/firestore';
+import Animated, { useSharedValue, useAnimatedStyle, withSpring, withTiming } from 'react-native-reanimated'
 
 
 
 
 const Login: React.FC = () => {
-    const handleSubmit = (values:any) => {
-console.log(values, 'login')
+    const initialIconVal = useSharedValue(0)
+    const animatedStyle = useAnimatedStyle(() => {
+        return {
+            transform: [
+                { scale: initialIconVal.value },
+                { translateX: initialIconVal.value },
+                { rotate: `${initialIconVal.value * (360 * 2)}deg` },
+            ]
+        }
+    })
+
+    useEffect(() => {
+        initialIconVal.value = withTiming(1, { duration: 2000 }), withSpring(1);
+    }, []);
+    const handleSubmit = (values: any) => {
+        firestore()
+            .collection('Users')
+            // Filter results
+            .where("email", '==', values.email)
+            .get()
+            .then(res => {
+                console.log("res", res?._docs[0]?._data.password)
+                if (res?._docs[0]?._data.password == values.password) {
+                    NavigationService.push(SCREENS.REGISTER);
+                }
+            }).catch((err) => {
+                console.log('user not found', err)
+            });
         // NavigationService.push(SCREENS.REGISTER);
     }
+
     return (
         <SafeAreaView style={authStyles.safeAreaView}>
             <ScrollView>
                 <View style={authStyles?.auth}>
-                    <View style={authStyles.logoView}>
+                    <Animated.View style={[authStyles.logoView, animatedStyle]}>
                         <FastImage
                             source={imgs?.Logo}
                             style={authStyles.logo}
                             resizeMode="contain"
                         />
-                    </View>
+                    </Animated.View>
                     <Form
                         submit={handleSubmit}
                     // handleForgot={handleForgot}
--- src/screens/authstack/register/Register.tsx
+++ src/screens/authstack/register/Register.tsx
@@ -1,4 +1,4 @@
-import { SafeAreaView, View } from 'react-native'
+import { SafeAreaView, View, Alert } from 'react-native'
 import React from 'react'
 import { Form } from './Form'
 import authStyles from '../authStyles'
@@ -9,11 +9,28 @@ import { Text } from 'react-native'
 import { SCREENS } from '@shared-constants'
 import * as NavigationService from "react-navigation-helpers";
 import RNBounceable from '@freakycoder/react-native-bounceable'
+import firestore from '@react-native-firebase/firestore';
+import uuid from 'react-native-uuid';
 
 
 const Register: React.FC = () => {
-    const handleSubmit = (value:any) => {
+    const userId: string | any = uuid.v4();
+    console.log("🚀 ~ file: Register.tsx:18 ~ userId:", userId)
+    const handleSubmit = (value: any) => {
         console.log('register', value)
+        firestore().collection('Users')
+            .doc(userId).set({
+                name: value?.name,
+                email: value?.email,
+                password: value?.password,
+                userId: userId
+            }).then(() => {
+                console.log('User added!');
+                Alert.alert('User added uccessfully')
+            }).catch(err => {
+                console.log(err, 'error-register')
+                Alert.alert('User added unsuccessfully')
+            });
     }
     return (
         <SafeAreaView style={authStyles.safeAreaView}>
@@ -33,7 +50,7 @@ const Register: React.FC = () => {
                     />
                     <View style={authStyles.bottomlink}>
                         <Text style={authStyles.bottomlinkText}>Already have an account? </Text>
-                        <RNBounceable  onPress={() => NavigationService.push(SCREENS.LOGIN)}>
+                        <RNBounceable onPress={() => NavigationService.push(SCREENS.LOGIN)}>
                             <Text style={authStyles.bottomlinkTextNav}>Login</Text>
                         </RNBounceable>
                     </View>
